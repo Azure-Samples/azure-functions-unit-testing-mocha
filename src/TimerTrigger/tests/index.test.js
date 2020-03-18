9d970b8c84f4e4d7b5bbc70b62d04e3638c6cf65
Munish Malhotra <munishm@outlook.com>
2020-03-18 17:58:35 +0530

    replaced var with let

--- src/TimerTrigger/tests/index.test.js
+++ src/TimerTrigger/tests/index.test.js
@@ -1,8 +1,8 @@
-var chai = require('chai');
+let chai = require('chai');
 const timerFunction = require('../index');
 const context = require('../testing/defaultContext');
 const timer = require('../testing/defaultTimer');
-var expect = chai.expect;
+let expect = chai.expect;
 it('Timer trigger should log message',  () => {
     timerFunction(context, timer);
     expect(context.log.callCount).to.equal(1);
